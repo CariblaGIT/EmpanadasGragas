cbb3c2104b38303524085a2aeced13bd50c7e894
Caribla <caribla4@gmail.com>
2024-02-10 13:45:30 +0100

    Algorithm simplified to all case scenario in front of each case scenario

--- main.js
+++ main.js
@@ -11,96 +11,11 @@ export const fn = (empanadasPollo, empanadasTernera, empanadasVerdura) => {
       throw new Error("El numero de empanadas pedido excede el de produccion");
    }
 
-   let totalCosto = 0;
    const precioPollo = 12;
    const precioTernera = 14;
    const precioVerdura = 16;
 
-   while(empanadasPollo > 0 && empanadasTernera > 0 && empanadasVerdura > 0){
-      totalCosto = totalCosto + 14;
-      empanadasPollo--;
-      empanadasTernera--;
-      empanadasVerdura--;
-   }
-
-   let hayPollo = (empanadasPollo) ? 1 : 0;
-   let hayTernera = (empanadasTernera) ? 1 : 0;
-   let hayVerdura = (empanadasVerdura) ? 1 : 0;
-
-   if(!hayPollo && !hayTernera){
-      while(empanadasVerdura > 0){
-         totalCosto = totalCosto + precioVerdura;
-         empanadasVerdura = empanadasVerdura - 3;
-      }
-   }
-
-   if(!hayVerdura && !hayPollo){
-      while(empanadasTernera > 0){
-         totalCosto = totalCosto + precioTernera;
-         empanadasTernera = empanadasTernera - 3;
-      }
-   }
-
-   if(!hayVerdura && !hayTernera){
-      while(empanadasPollo > 0){
-         totalCosto = totalCosto + precioPollo;
-         empanadasPollo = empanadasPollo - 3;
-      }
-   }
-
-   if(!hayPollo){
-      while(empanadasTernera > 0 && empanadasVerdura > 0){
-         if(empanadasTernera < empanadasVerdura){
-            totalCosto = totalCosto + precioVerdura;
-            empanadasTernera = empanadasTernera - 1;
-            empanadasVerdura = empanadasVerdura - 2;
-         } else if (empanadasTernera > empanadasVerdura){
-            totalCosto = totalCosto + (precioVerdura + precioTernera)/2;
-            empanadasTernera = empanadasTernera - 2;
-            empanadasVerdura = empanadasVerdura - 1;
-         } else {
-            totalCosto = totalCosto + precioVerdura + precioTernera;
-            empanadasVerdura = empanadasVerdura - 3;
-            empanadasTernera = empanadasTernera - 3;
-         }
-      }
-   }
-
-   if(!hayTernera){
-      while(empanadasPollo > 0 && empanadasVerdura > 0){
-         if(empanadasPollo < empanadasVerdura){
-            totalCosto = totalCosto + precioVerdura;
-            empanadasPollo = empanadasPollo - 1;
-            empanadasVerdura = empanadasVerdura - 2;
-         } else if (empanadasPollo > empanadasVerdura){
-            totalCosto = totalCosto + (precioPollo + precioVerdura)/2;
-            empanadasPollo = empanadasPollo - 2;
-            empanadasVerdura = empanadasVerdura - 1;
-         } else {
-            totalCosto = totalCosto + precioVerdura + precioPollo;
-            empanadasVerdura = empanadasVerdura - 3;
-            empanadasPollo = empanadasPollo - 3;
-         }
-      }
-   }
-
-   if(!hayVerdura){
-      while(empanadasPollo > 0 && empanadasTernera > 0){
-         if(empanadasPollo < empanadasTernera){
-            totalCosto = totalCosto + precioTernera;
-            empanadasPollo = empanadasPollo - 1;
-            empanadasTernera = empanadasTernera - 2;
-         } else if (empanadasPollo > empanadasTernera){
-            totalCosto = totalCosto + (precioPollo + precioTernera)/2;
-            empanadasPollo = empanadasPollo - 2;
-            empanadasTernera = empanadasTernera - 1;
-         } else {
-            totalCosto = totalCosto + precioPollo + precioTernera;
-            empanadasTernera = empanadasTernera - 3;
-            empanadasPollo = empanadasPollo - 3;
-         }
-      }
-   }
-
+   let totalCosto = Math.ceil((empanadasPollo * precioPollo + empanadasTernera * precioTernera + empanadasVerdura * precioVerdura) / 3);
+   
    return totalCosto;
 };
